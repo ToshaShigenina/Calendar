ce637d290e97698d54f24a3205af31292c96fac6
Tosha Shigenina <shigenina@gmail.com>
2020-05-12 14:31:32 +0500

    Создана структура для хранения данных календаря. Добавлены дата-атрибуты в календарь.

--- main.js
+++ main.js
@@ -1,53 +1,75 @@
 document.addEventListener('DOMContentLoaded', () => {
-  const body = document.body,
-    data = {};
-
-  const editControl = () => {
-    const controlInput = document.querySelector('.control__input'),
-      taskList = document.querySelector('.task__active');
-
-    body.addEventListener('click', (event) => {
-      let target = event.target;
-
-      if (target.closest('.button__add')) {
-        if (target.nextElementSibling.closest('.control__input') && !target.classList.contains('button__delete')) {
-          target.classList.add('button__delete');
-          target.nextElementSibling.classList.add('control__edit');
-        } else {
-          controlInput.value = '';
-          target.classList.remove('button__delete');
-          target.nextElementSibling.classList.remove('control__edit');
+  const data = {
+    day05052020: [
+      {
+        id: 'task0505202013451',
+        content: 'Задача 1',
+        check: false
+        },
+      {
+        id: 'task0505202014202',
+        content: 'Задача 2',
+        check: true
+        },
+      {
+        id: 'task0505202015393',
+        content: 'Задача 3',
+        check: false
         }
-      }
+      ],
+    day06052020: [],
+    day07052020: [
+      {
+        id: 'task0705202013451',
+        content: 'Задача 1',
+        check: true
+        },
+      {
+        id: 'task0705202014202',
+        content: 'Задача 2',
+        check: true
+        },
+      {
+        id: 'task0705202015393',
+        content: 'Задача 3',
+        check: false
+        }
+      ],
+    day08052020: []
+  };
 
-      if (target.closest('.control .button__check') && controlInput.value) {
-        let task = document.createElement('li');
-        task.classList.add('task__item');
-        task.innerHTML = `<button class="button button__check"></button><span class="task__content"></span><button class="button button__delete"></button>`;
-        task.querySelector('.task__content').textContent = controlInput.value;
-        taskList.prepend(task);
+  const body = document.body,
+    control = document.querySelector('.control'),
+    controlInput = control.querySelector('.control__input'),
+    taskActive = document.querySelector('.task__active'),
+    taskCheck = document.querySelector('.task__check');
+
+  const editControl = (event) => {
+    let target = event.target;
+
+    if (target.closest('.button__add')) {
+      if (target.nextElementSibling.closest('.control__input') && !target.classList.contains('button__delete')) {
+        target.classList.add('button__delete');
+        target.nextElementSibling.classList.add('control__edit');
+      } else {
         controlInput.value = '';
-        target.previousElementSibling.classList.remove('control__edit');
-        document.querySelector('.control .button__add').classList.remove('button__delete');
+        target.classList.remove('button__delete');
+        target.nextElementSibling.classList.remove('control__edit');
       }
-    });
+    }
+
+    if (target.closest('.button__check') && controlInput.value) {
+      let task = document.createElement('li');
+      task.classList.add('task__item');
+      task.innerHTML = `<button class="button button__check"></button><span class="task__content"></span><button class="button button__delete"></button>`;
+      task.querySelector('.task__content').textContent = controlInput.value;
+      taskActive.prepend(task);
+      controlInput.value = '';
+      target.previousElementSibling.classList.remove('control__edit');
+      document.querySelector('.control .button__add').classList.remove('button__delete');
+    }
   };
 
-  editControl();
-
-  const editTask = () => {
-    const listActive = document.querySelector('.task__active'),
-      listCheck = document.querySelector('.task__check'),
-      dashboard = document.querySelector('.dashboard');
-
-    dashboard.addEventListener('click', (event) => {
-      let target = event.target;
-
-
-    });
-
-  };
-
-  editTask();
+  control.addEventListener('click', editControl);
 
 });
